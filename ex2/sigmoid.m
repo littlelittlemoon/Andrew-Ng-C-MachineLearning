66b75a73aaea5b8be2f59e3f9d82b693f10c8be5
TanKaiyue <littlemeemoon@gmail.com>
2019-01-23 12:11:56 +0800

    modify sigmoid function

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -5,6 +5,6 @@ function g = sigmoid(z)
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-g = 1 / (1 + exp(-z));
+g = sigmf(z, [1, 0]);
 
 end
